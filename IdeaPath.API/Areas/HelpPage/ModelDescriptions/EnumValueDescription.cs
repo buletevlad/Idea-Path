9d2c478f37bd3968417bd47a51378b0f99abe128
Vlad <buletevlad@yahoo.com>
2018-02-10 17:23:32 +0200

    added Api, Dal, Bll

---
+++ IdeaPath.API/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace IdeaPath.API.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
